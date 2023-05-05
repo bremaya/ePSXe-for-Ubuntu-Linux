df0a69c648b02d116c744b31b36463f313c37dd4
Breno Maia <39398165+bremaya@users.noreply.github.com>
2023-05-05 18:35:06 -0300

    Update epsxe_online_uninstaller.sh
    Minor changes

--- epsxe_online_uninstaller.sh
+++ epsxe_online_uninstaller.sh
@@ -9,7 +9,7 @@ ARC=$(uname -m)
 if [ $ARC = x86_64 ]
   then
 #ePSXe Libs x86_64 Remove
-   sudo dpkg -P libncurses5 libncurses5:i386 libsdl1.2debian libsdl1.2debian:i386 libsdl-ttf2.0-0 libsdl-ttf2.0-0:i386 libgssapi3-heimdal libgssapi3-heimdal:i386 libssl1.0.0 libssl1.0.0:i386
+   sudo dpkg -P libssl1.0.0 libssl1.0.0:i386 libncurses5 libncurses5:i386 libsdl1.2debian libsdl1.2debian:i386 libsdl-ttf2.0-0 libsdl-ttf2.0-0:i386 libgssapi3-heimdal libgssapi3-heimdal:i386 libgtk-3-0 libgtk-3-0:i386
    sudo rm -fv /usr/lib/x86_64-linux-gnu/libcurl.so.3 /usr/lib/i386-linux-gnu/libcurl.so.3 /usr/lib/x86_64-linux-gnu/libcurl.so.4.5.0 /usr/lib/i386-linux-gnu/libcurl.so.4.5.0 /usr/lib/x86_64-linux-gnu/liblber-2.4.so.2 /usr/lib/i386-linux-gnu/liblber-2.4.so.2 /usr/lib/x86_64-linux-gnu/liblber-2.4.so.2.10.12 /usr/lib/i386-linux-gnu/liblber-2.4.so.2.10.12 /usr/lib/x86_64-linux-gnu/libldap-2.4.so.2 /usr/lib/i386-linux-gnu/libldap-2.4.so.2 /usr/lib/x86_64-linux-gnu/libldap_r-2.4.so.2 /usr/lib/i386-linux-gnu/libldap_r-2.4.so.2 /usr/lib/x86_64-linux-gnu/libldap_r-2.4.so.2.10.12 /usr/lib/i386-linux-gnu/libldap_r-2.4.so.2.10.12
 #ePSXe x86_64 Remove
    sudo rm -fv /usr/bin/epsxe /usr/bin/epsxe_x86 /usr/share/icons/epsxe.svg /usr/share/applications/epsxe.desktop
@@ -18,7 +18,7 @@ if [ $ARC = x86_64 ]
 "
   else
    #ePSXe Libs x86 Remove
-   sudo dpkg -P libncurses5 libsdl1.2debian libsdl-ttf2.0-0 libgssapi3-heimdal libssl1.0.0
+   sudo dpkg -P libssl1.0.0 libncurses5 libsdl1.2debian libsdl-ttf2.0-0 libgssapi3-heimdal libgtk-3-0
    sudo rm -fv /usr/lib/i386-linux-gnu/libcurl.so.3 /usr/lib/i386-linux-gnu/libcurl.so.4.5.0 /usr/lib/i386-linux-gnu/liblber-2.4.so.2 /usr/lib/i386-linux-gnu/liblber-2.4.so.2.10.12 /usr/lib/i386-linux-gnu/libldap-2.4.so.2 /usr/lib/i386-linux-gnu/libldap_r-2.4.so.2 /usr/lib/i386-linux-gnu/libldap_r-2.4.so.2.10.12
 #ePSXe x86_64 Remove
    sudo rm -fv /usr/bin/epsxe /usr/share/icons/epsxe.svg /usr/share/applications/epsxe.desktop
